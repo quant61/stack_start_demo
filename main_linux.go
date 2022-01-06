b23479a6e0481dca5b0244d3b9c5fda25c8efc00
kwant <quant61@yandex.ru>
2022-01-06 15:26:53 +0300

    make it possible to choose single binary

--- main_linux.go
+++ main_linux.go
@@ -1,15 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
 	//run()
 	// TODO: add more platforms
 	// TODO: make it configurable
 	// TODO: add foreign platform support
-	//
-	fmt.Println("RUN FOR 32-BIT")
-	elfRun(buildElfBinary32, 4)
-	fmt.Println("RUN FOR 64-BIT")
-	elfRun(buildElfBinary64, 8)
+	mode := flag.String("mode", "both", "32 or 64-bit mode")
+	flag.Parse()
+
+	switch *mode {
+	case "32":
+		fmt.Println("RUN FOR 32-BIT")
+		elfRun(buildElfBinary32, 4)
+	case "64":
+		fmt.Println("RUN FOR 64-BIT")
+		elfRun(buildElfBinary64, 8)
+	case "both":
+		fmt.Println("RUN FOR 32-BIT")
+		elfRun(buildElfBinary32, 4)
+		fmt.Println("RUN FOR 64-BIT")
+		elfRun(buildElfBinary64, 8)
+	}
 }
